b7d13f3e6ab103aa1c079197ae0bb1fb130417a0
Ezekiel Grosfeld <ezekiel.grosfeld@r2capital.co>
2023-11-15 18:34:54 -0300

    add new configurations

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source $HOME/.config/fish/abbreviations.fish
+end
+
+# NOTE: allow expanding commands using C-e
+bind -M insert \cE forward-char
+
+# NOTE: disables "Welcome to Fish" greeting
+set fish_greeting
+
+set -gx DEFAULT_USER $USER
+set -gx EDITOR 'nvim'
+set -gx HSANDBOX_EDITOR 'nvim'
+set -gx GIT_EDITOR 'nvim'
+
+fish_add_path "$HOME/bin"
+fish_add_path "/usr/local/bin"
+fish_add_path "$HOME/go/bin"
+fish_add_path /opt/homebrew/bin
+
+set -gx HOMEBREW_PREFIX "/opt/homebrew" # lookup using (brew --prefix)
