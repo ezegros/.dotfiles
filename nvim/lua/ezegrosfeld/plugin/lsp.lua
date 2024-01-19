d166b632fd6cc601bfb407e481377294350ccae7
ezegrosfeld <grosfeldezekiel@gmail.com>
2024-01-19 20:56:54 -0300

    try fix lsp

--- nvim/lua/ezegrosfeld/plugin/lsp.lua
+++ nvim/lua/ezegrosfeld/plugin/lsp.lua
@@ -1,76 +1,73 @@
 return {
     {
         'williamboman/mason.nvim',
-        opts = {
-            ensure_installed = { 'lua_ls', 'rust_analyzer', 'tsserver', 'gopls' },
-        }
-    },
-    {
-        'williamboman/mason-lspconfig.nvim',
-        opts = {
-        }
-    },
-    {
-        'neovim/nvim-lspconfig',
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+        },
+        lazy = false,
         config = function()
-            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-            vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float)
-            -- Use LspAttach autocommand to only map the following keys
-            -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = function(ev)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
 
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set('n', '<leader>rr', vim.lsp.buf.references, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', '<leader>f', function()
-                        vim.lsp.buf.format { async = true }
-                    end, opts)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                desc = "Lsp Actions",
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+
+                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
                 end,
             })
-        end
-    },
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-        },
-        opts = {
-            snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            window = {
-                -- completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
-            },
-        },
-        config = function()
+
+            mason.setup({})
+            mason_lspconfig.setup({
+                ensure_installed = { 'lua_ls', 'rust_analyzer', 'tsserver', 'gopls' },
+            })
+
+            lspconfig.lua_ls.setup {
+                on_attach = lspconfig.util.on_attach,
+                capabilities = capabilities
+            }
+
+            lspconfig.rust_analyzer.setup {
+                on_attach = lspconfig.util.on_attach,
+                capabilities = capabilities
+            }
+
+            lspconfig.tsserver.setup {
+                on_attach = lspconfig.util.on_attach,
+                capabilities = capabilities
+            }
+
+            lspconfig.gopls.setup {
+                on_attach = lspconfig.util.on_attach,
+                capabilities = capabilities,
+            }
+
             local cmp = require 'cmp'
             -- Set configuration for specific filetype.
             cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
                 mapping = cmp.mapping.preset.insert({
                     ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-e>'] = cmp.mapping.abort(),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
@@ -86,57 +83,6 @@ return {
                     { name = 'buffer' },
                 })
             })
-
-            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                    { name = 'cmdline' }
-                })
-            })
-
-            -- Set up lspconfig.
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require('lspconfig')
-            -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-            lspconfig.lua_ls.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-            lspconfig.rust_analyzer.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-            lspconfig.tsserver.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-            lspconfig.gopls.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities,
-                cmd = { 'gopls' },
-                filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-                root_dir = lspconfig.util.root_pattern('go.mod', '.git'),
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            unusedparams = true,
-                        },
-                    },
-                },
-            }
         end
     }
 }
