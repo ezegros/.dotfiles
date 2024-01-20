bd56e6a72bab075cf01f38f03f7011ac070b5cb6
ezegrosfeld <grosfeldezekiel@gmail.com>
2024-01-20 18:49:33 -0300

    go back to lsp zero

--- nvim/lua/ezegrosfeld/plugin/lsp.lua
+++ nvim/lua/ezegrosfeld/plugin/lsp.lua
@@ -1,93 +1,76 @@
 return {
+    { 'williamboman/mason.nvim' },
+
+    { 'williamboman/mason-lspconfig.nvim' },
+
     {
-        'williamboman/mason.nvim',
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp",
-            "L3MON4D3/LuaSnip",
-        },
-        lazy = false,
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
         config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local mason = require("mason")
-            local mason_lspconfig = require("mason-lspconfig")
+            local lsp = require('lsp-zero').preset({})
+            lsp.extend_lspconfig()
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                desc = "Lsp Actions",
-                callback = function(event)
-                    local opts = { buffer = event.buf }
+            lsp.on_attach(function(_, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp.default_keymaps({ buffer = bufnr })
+                local opts = { buffer = bufnr, remap = false }
 
-                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-                    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-                    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-                end,
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            end)
+
+            lsp.set_sign_icons({
+                error = 'E',
+                warn = 'W',
+                hint = 'H',
+                info = 'I'
             })
 
-            mason.setup({})
-            mason_lspconfig.setup({
-                ensure_installed = { 'lua_ls', 'rust_analyzer', 'tsserver', 'gopls' },
-            })
-
-            lspconfig.lua_ls.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-
-            lspconfig.rust_analyzer.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-
-            lspconfig.tsserver.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities
-            }
-
-            lspconfig.gopls.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities,
-            }
-
-            lspconfig.bashls.setup {
-                on_attach = lspconfig.util.on_attach,
-                capabilities = capabilities,
-            }
-
-            local cmp = require 'cmp'
-            -- Set configuration for specific filetype.
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = { 'lua_ls', 'rust_analyzer' },
+                handlers = {
+                    lsp.default_setup,
+                    lua_ls = function()
+                        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            })
+
+            lsp.setup()
+
+            local cmp = require('cmp')
+            local cmp_action = require('lsp-zero').cmp_action()
+
+            cmp.setup({
+                mapping = {
+                    -- `Enter` key to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+                    -- Ctrl+Space to trigger completion menu
                     ['<C-Space>'] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'buffer' },
-                })
+
+                    -- Navigate between snippet placeholder
+                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                }
             })
-            cmp.setup.filetype('gitcommit', {
-                sources = cmp.config.sources({
-                    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-                }, {
-                    { name = 'buffer' },
-                })
-            })
-        end
-    }
+        end,
+    },
+
+    { 'neovim/nvim-lspconfig' },
+
+    { 'hrsh7th/cmp-nvim-lsp' },
+
+    { 'hrsh7th/nvim-cmp' },
+
+    { 'L3MON4D3/LuaSnip' }
 }
