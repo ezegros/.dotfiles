e14ed9348750a902bc91559d3c9301e3f55e9737
Ezekiel Grosfeld <ezekiel.grosfeld@r2capital.co>
2024-02-01 13:16:16 -0300

    add neotest & small fixes

--- nvim/lua/ezegrosfeld/plugin/harpoon.lua
+++ nvim/lua/ezegrosfeld/plugin/harpoon.lua
@@ -10,9 +10,7 @@ return {
     config = function()
         local harpoon = require("harpoon")
 
-        -- REQUIRED
         harpoon:setup({})
-        -- REQUIRED
 
         vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
+++ nvim/lua/ezegrosfeld/plugin/neotest.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-go",
+        "rouge8/neotest-rust",
+    },
+    config = function()
+        local neotest = require("neotest")
+        neotest.setup({
+            adapters = {
+                require("neotest-rust"),
+                require("neotest-go"),
+            }
+        })
+
+        vim.keymap.set("n", "<leader>tr", function()
+            neotest.run.run()
+        end)
+
+        vim.keymap.set("n", "<leader>tf", function()
+            neotest.run.run(vim.fn.expand("%"))
+        end)
+    end
+}
