f4feaac857810057dc931f3a4a9bfaa5d9d59ffd
ezegros <grosfeldezekiel@gmail.com>
2025-04-05 19:55:44 -0300

    feat: enhace test & debug

--- nvim/lua/ezegrosfeld/plugin/dap.lua
+++ nvim/lua/ezegrosfeld/plugin/dap.lua
@@ -1,6 +1,8 @@
 return {
     { 'leoluz/nvim-dap-go',              opts = {} },
+
     'rcarriga/nvim-dap-ui',
+
     { 'theHamsta/nvim-dap-virtual-text', opts = {} },
 
     {
@@ -10,6 +12,27 @@ return {
             local dapui = require('dapui')
 
             dapui.setup()
+
+            dap.adapters.codelldb = {
+                type = "executable",
+                command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+                -- On windows you may have to uncomment this:
+                -- detached = false,
+            }
+
+            dap.configurations.zig = {
+                {
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                },
+            }
 
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open()
--- nvim/lua/ezegrosfeld/plugin/neotest.lua
+++ nvim/lua/ezegrosfeld/plugin/neotest.lua
@@ -5,24 +5,49 @@ return {
         "nvim-lua/plenary.nvim",
         "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
-        "nvim-neotest/neotest-go",
         "rouge8/neotest-rust",
+        "fredrikaverpil/neotest-golang",
+        "leoluz/nvim-dap-go",
     },
     config = function()
         local neotest = require("neotest")
         neotest.setup({
             adapters = {
                 require("neotest-rust"),
-                require("neotest-go"),
+                require("neotest-golang")({
+                    dap = { justMyCode = false },
+                }),
             }
         })
 
         vim.keymap.set("n", "<leader>tr", function()
-            neotest.run.run()
+            neotest.run.run({
+                suite = false,
+                testify = true,
+            })
         end)
 
         vim.keymap.set("n", "<leader>tf", function()
             neotest.run.run(vim.fn.expand("%"))
+        end)
+
+        vim.keymap.set("n", "<leader>ts", function()
+            neotest.run.run({
+                suite = true,
+                testify = true,
+            })
+        end)
+
+        vim.keymap.set("n", "<leader>tv", function()
+            neotest.summary.toggle()
+        end)
+
+        vim.keymap.set("n", "<leader>td", function()
+            neotest.run.run({
+                suite = false,
+                testify = true,
+                strategy = "dap",
+            })
         end)
     end
 }
